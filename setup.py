68fa2f999348c1a2488ed6ab4b7f534db77286c6
Bernd Dorn <bernddorn@gmail.com>
2007-03-25 10:04:49 +0000

    deps, not test, so should be ready

--- setup.py
+++ setup.py
@@ -31,8 +31,10 @@ setup(name='zope.app.debug',
       package_dir = {'': 'src'},
       namespace_packages=['zope', 'zope.app'],
       extras_require = dict(test=['zope.app.testing']),
-      install_requires=['setuptools'
-                        'zope.interface',
+      install_requires=['setuptools',
+                        'zope.publisher',
+                        'zope.app.appsetup',
+                        'zope.app.publication'
                         ],
       include_package_data = True,
       zip_safe = False,
