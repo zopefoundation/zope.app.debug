7cfb407344676bf0877b551249b638ea32ba60b1
Jason Madden <jamadden@gmail.com>
2017-05-08 05:45:36 -0500

    Preparing release 4.0.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ tests_require = [
 ]
 
 setup(name='zope.app.debug',
-      version='4.0.0.dev0',
+      version='4.0.0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Debug Mode',
